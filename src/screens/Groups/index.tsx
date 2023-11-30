33a3376ac5d36e7ebf59baef2b4adba0e19e4391
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-11-29 22:59:09 -0300

    :construction: list groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,31 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { Header } from '@components/Header'
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
+  const [ groups, setGroups ] = useState<string[]>([
+    'Rocketseat Team',
+    'Dev team'
+  ])
+  
   return (
     <Container>
       <Header />
 
       <Highlight title='Turmas' subtitle='Jogue com a sua turma'/>
 
-      <GroupCard title='Turma 11' onPress={() => {}}/>
+      <FlatList
+        style={{ width: '100%' }} 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => {}}/>
+        )}/>
+
     </Container>
   )
 }
