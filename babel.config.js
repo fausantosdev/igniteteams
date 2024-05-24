0c965ac0be7bdc2192b8363ac72323d3210a5280
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-05-23 21:27:33 -0300

    :wrench: add the theme alias to the path

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = function(api) {
             '@routes': './src/routes',
             '@screens': './src/screens',
             '@storage': './src/storage',
+            '@theme': './src/theme',
             '@utils': './src/utils'
           }
         }
